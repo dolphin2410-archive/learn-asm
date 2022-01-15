bb20ac56780df5917e723eb0ca66679a870ffd3d
dolphin2410 <dolphin2410@outlook.kr>
2022-01-16 00:23:55 +0900

    Strings and Newline

--- complex_boot.asm
+++ complex_boot.asm
@@ -1,26 +1,27 @@
 ORG 0x7c00
 
 mov bx, welcome
+call print_string		; Print
 call print_string
 
 jmp $
 
 print_string:
-	pusha
-	mov ah, 0x0e
-	mov al, [bx]
+	pusha			; Push All
+	mov ah, 0x0e		; Write mode
+	mov al, [bx]		; The index of the first char
 	loop:
-		cmp al, 0
-		je break
-		int 0x10
-		add bx, 0x01
-		mov al, [bx]
-		jmp loop
+		cmp al, 0	; On String End
+		je break	; Break
+		int 0x10	; Interrupt
+		add bx, 0x01	; The next string index
+		mov al, [bx]	; Char at the index 'bx'
+		jmp loop	; Loop
 	break:
-		popa
-		ret
+		popa		; Pop All
+		ret		; Return
 
-welcome db 'Hello, World!',0
+welcome db 'Hello, World!',0x0a,0x0d,0
 
 times 510-($-$$) db 0
 dw 0xaa55
